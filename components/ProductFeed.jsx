1cb0aa221c755a0fded6e35f64953bd094a25927
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-10-16 20:38:06 +0100

    products layouts

--- components/ProductFeed.jsx
+++ components/ProductFeed.jsx
@@ -4,7 +4,23 @@ import Product from './Product';
 const ProductFeed = ({ products }) => {
   return (
     <div className='md:-mt-36 grid grid-flow-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-      {products.map((prod) => (
+      {products.slice(0, 4).map((prod) => (
+        <Product product={prod} key={prod.id} />
+      ))}
+
+      <img
+        loading='lazy'
+        className='md:col-span-full'
+        src='https://links.papareact.com/dyz'
+        alt=''
+      />
+      <div className='md:col-span-2 lg:col-span-1'>
+        {products.slice(4, 5).map((prod) => (
+          <Product product={prod} key={prod.id} />
+        ))}
+      </div>
+
+      {products.slice(6).map((prod) => (
         <Product product={prod} key={prod.id} />
       ))}
     </div>
