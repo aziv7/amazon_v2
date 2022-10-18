c501f1b4f5dd71e78824d6ae2dd5928d97e75e03
aziv7 <aziz.jerbi@esprit.tn>
2022-10-18 22:32:06 +0200

    checkout product component

---
+++ components/CheckoutProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function CheckoutProduct({
+  product: { id, category, description, image, price, rating, title },
+}) {
+  return <div></div>;
+}
+
+export default CheckoutProduct;
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { CheckoutProduct } from '../components';
 import Header from '../components/Header';
+import { selectItems } from '../slices/cartReducer';
 
 const Checkout = () => {
+  const items = useSelector(selectItems);
+
   return (
     <div className='bg-gray-100'>
       <Header />
@@ -17,7 +22,13 @@ const Checkout = () => {
           />
         </div>
         <div className='flex flex-col p-5 space-y-10 bg-white '>
-          <h1 className='text-3xl border-b pb-4'>Shopping Cart</h1>
+          <h1 className='text-3xl border-b pb-4'>
+            {items.length > 0 ? 'Shopping Cart' : 'Shopping Cart is empty'}{' '}
+            Shopping Cart
+          </h1>
+          {items.map((item) => (
+            <CheckoutProduct product={item} key={item.id} />
+          ))}
         </div>
 
         {/**right section */}
